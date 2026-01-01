47c43569ce3e5857fd009e8d09d56fd5bb72fb2b
rui.meng <mengrui6351@gmail.com>
2026-01-01 23:37:18 +0000

    reformulate job submision scripts.

--- scripts/mm_gold/mm_gold_qwen3vl_CT_RL.sh
+++ scripts/mm_gold/mm_gold_qwen3vl_CT_RL.sh
@@ -2,9 +2,9 @@
 
 # Multimodal Gold training script for Qwen3-VL
 
-export CUDA_VISIBLE_DEVICES=7,6
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 
-torchrun --nproc_per_node=2 \
+torchrun --nproc_per_node=4 \
          --nnodes=1 \
          --node_rank=0 \
          --master_addr="127.0.0.1" \
@@ -14,6 +14,8 @@ torchrun --nproc_per_node=2 \
          --deepspeed src/training_configs/zero3.json \
          --teacher_model_name_or_path Qwen/Qwen3-VL-4B-Instruct \
          --dataset_name "/mnt/task_runtime/data/omni_med_vqa_processed/open_access_sft_data_hf_modality_CT(Computed_Tomography)_train" \
+         --dataset_type vqa_thinking \
+         --learning_rate 1e-6 \
          --logging_steps 1 \
          --bf16 true \
          --per_device_train_batch_size 2 \
@@ -21,6 +23,8 @@ torchrun --nproc_per_node=2 \
          --output_dir gold-model-RL \
          --num_train_epochs 1 \
          --alpha 0.0 \
-         --num_generations 6 \
+         --num_generations 8 \
          --attn_implementation flash_attention_2 \
-         --reward_funcs accuracy format
+         --reward_funcs accuracy format \
+         --run_name Qwen3-VL-2B-MM-GOLD-GRPO-CT-Think \
+         --push_to_hub True
