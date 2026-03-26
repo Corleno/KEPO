80449a61a2c92387f86881f7477ad4bb3468e577
root <root@bolt-vtn65qqcu6-5cpfkyk6av.bolt-pods.turi-bolt.svc.cluster.local>
2026-03-26 04:49:37 +0000

    refactor trainer class names to unify naming convention for GRPO trainers

--- src/r1-v/src/open_r1/trainer/__init__.py
+++ src/r1-v/src/open_r1/trainer/__init__.py
@@ -1,4 +1,4 @@
-from .grpo_trainer import Qwen2VLGRPOTrainer
+from .grpo_trainer import QwenVLGRPOTrainer
 from .vllm_grpo_trainer import Qwen2VLGRPOVLLMTrainer 
 from .grpo_trainer_mmiq import Qwen2VLGRPOTrainerMMIQ
-__all__ = ["Qwen2VLGRPOTrainer", "Qwen2VLGRPOVLLMTrainer", "Qwen2VLGRPOTrainerMMIQ"]
+__all__ = ["QwenVLGRPOTrainer", "Qwen2VLGRPOVLLMTrainer", "Qwen2VLGRPOTrainerMMIQ"]
